0882a722642f75c89ba21cda8dee55c96d133753
Ayush898 <33051788+Ayush898@users.noreply.github.com>
2017-10-26 17:14:59 +0530

    Add files via upload
    Basic Codes for the transmitter and receiver.

---
+++ rx_base.ino
@@ -0,0 +1,29 @@
+#include <VirtualWire.h>
+const int receive_pin = 11;
+void setup()
+{
+    delay(1000);
+    Serial.begin(9600);
+    Serial.println("setup");
+    vw_set_rx_pin(receive_pin);
+    vw_set_ptt_inverted(true);
+    vw_setup(2000);
+    vw_rx_start();
+}
+void loop()
+{
+    uint8_t buf[VW_MAX_MESSAGE_LEN];
+    uint8_t buflen = VW_MAX_MESSAGE_LEN;
+    if (vw_get_message(buf, &buflen))
+    {
+  int i;       
+  Serial.print("Got: ");
+  for (i = 0; i < buflen; i++)
+  {
+      Serial.print(buf[i], HEX);
+      Serial.print(' ');
+  }
+  Serial.println();
+    }
+}
+
---
+++ tx_base.ino
@@ -0,0 +1,20 @@
+#include <VirtualWire.h>
+const int transmit_pin = 12;
+const int transmit_en_pin = 3;
+void setup()
+{
+  vw_set_tx_pin(transmit_pin);
+  vw_set_ptt_pin(transmit_en_pin);
+  vw_set_ptt_inverted(true);
+  vw_setup(2000);
+}
+byte count = 1;
+void loop()
+{
+  char msg[7] = {'h','e','l','l','o',' ','#'};
+  msg[6] = count;
+  vw_send((uint8_t *)msg, 7);
+  vw_wait_tx();
+  delay(1000);
+  count = count + 1;
+}
