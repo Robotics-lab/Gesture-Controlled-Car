a173a6d571dc8ca1bc91ea31573b935229d079b9
Ayush898 <neowhitehouse@gmail.com>
2017-10-27 16:47:27 +0530

    Replacing Older Codes.

--- Receiver.ino
+++ Receiver.ino
@@ -1,78 +1,84 @@
 #include <VirtualWire.h>
-#include <VirtualWire_Config.h>
-
-int m1pin1 = 5;
-int m1pin2 = 6;
-int m2pin1 = 7;
-int m2pin2 = 8;
-
-void setup() {
-Serial.begin(9600);
-pinMode(5,OUTPUT);
-pinMode(6,OUTPUT);
-pinMode(7,OUTPUT);
-pinMode(8,OUTPUT);
-pinMode(12,INPUT);
-vw_set_ptt_inverted(true);
-vw_set_rx_pin(12);
-vw_setup(4000);
-vw_rx_start();
-}
-
-void loop() {
-uint8_t buf[VW_MAX_MESSAGE_LEN];
-uint8_t buflen = VW_MAX_MESSAGE_LEN;
-
-if(vw_get_message(buf,&buflen))
+const int receive_pin = 11;
+const int m1pin1 = 5;
+const int m1pin2 = 6;
+const int m2pin1 = 7;
+const int m2pin2 = 8;
+void setup()
 {
-  if (buf[0]=='61')
-  {
-    digitalWrite(m1pin1,1);
-    digitalWrite(m1pin2,0);
-    digitalWrite(m2pin1,1);
-    digitalWrite(m2pin2,0);
-  }
-  else if (buf[0]=='64')
-  {
-    digitalWrite(m1pin1,0);
-    digitalWrite(m1pin2,0);
-    digitalWrite(m2pin1,1);
-    digitalWrite(m2pin2,0); 
-  }
-  else if (buf[0]=='63')
-  {
-    digitalWrite(m1pin1,1);
-    digitalWrite(m1pin2,0);
-    digitalWrite(m2pin1,0);
-    digitalWrite(m2pin2,0);
-  }
-  else if (buf[0]=='62')
-  {
-    digitalWrite(m1pin1,0);
-    digitalWrite(m1pin2,1);
-    digitalWrite(m2pin1,0);
-    digitalWrite(m2pin2,1);
-  }
-  else if (buf[0]=='65')
-  {
-    digitalWrite(m1pin1,0);
-    digitalWrite(m1pin2,1);
-    digitalWrite(m2pin1,0);
-    digitalWrite(m2pin2,0);
-  }
-  else if (buf[0]=='66')
-  {
-    digitalWrite(m1pin1,0);
-    digitalWrite(m1pin2,0);
-    digitalWrite(m2pin1,0);
-    digitalWrite(m2pin2,1);
-  }
-  else  if (buf[0]=='67')
-  {
-    digitalWrite(m1pin1,0);
-    digitalWrite(m1pin2,0);
-    digitalWrite(m2pin1,0);
-    digitalWrite(m2pin2,0);    
-  }
+    Serial.begin(9600);
+    Serial.println("setup");
+    pinMode(5,OUTPUT);
+    pinMode(6,OUTPUT);
+    pinMode(7,OUTPUT);
+    pinMode(8,OUTPUT);
+    vw_set_rx_pin(receive_pin);
+    vw_set_ptt_inverted(true);
+    vw_setup(2000);
+    vw_rx_start();
 }
-}
+void loop()
+{
+ uint8_t buf[VW_MAX_MESSAGE_LEN];
+ uint8_t buflen = VW_MAX_MESSAGE_LEN;
+ if (vw_get_message(buf, &buflen))
+  {
+   int i;       
+   for (i = 0; i < buflen; i++)
+   {
+     int j;
+     j = buf[i];
+     Serial.println(j);
+     if (j == 97)
+     {
+      digitalWrite(m1pin1,1);
+      digitalWrite(m1pin2,0);
+      digitalWrite(m2pin1,1);
+      digitalWrite(m2pin2,0);
+     } 
+     else if (j == 100)
+     {
+      digitalWrite(m1pin1,0);
+      digitalWrite(m1pin2,0);
+      digitalWrite(m2pin1,1);
+      digitalWrite(m2pin2,0); 
+     }
+     else if (j == 99)
+     {
+      digitalWrite(m1pin1,1);
+      digitalWrite(m1pin2,0);
+      digitalWrite(m2pin1,0);
+      digitalWrite(m2pin2,0);
+     }
+     else if (j == 98)
+     {
+      digitalWrite(m1pin1,0);
+      digitalWrite(m1pin2,1);
+      digitalWrite(m2pin1,0);
+      digitalWrite(m2pin2,1);
+     }
+     else if (j == 101)
+     {
+      digitalWrite(m1pin1,0);
+      digitalWrite(m1pin2,1);
+      digitalWrite(m2pin1,0);
+      digitalWrite(m2pin2,0);
+     }
+     else if (j == 102)
+     {
+      digitalWrite(m1pin1,0);
+      digitalWrite(m1pin2,0);
+      digitalWrite(m2pin1,0);
+      digitalWrite(m2pin2,1);
+     }
+     else 
+     {
+      digitalWrite(m1pin1,0);
+      digitalWrite(m1pin2,0);
+      digitalWrite(m2pin1,0);
+      digitalWrite(m2pin2,0);
+     }     
+   }
+ }
+} 
+
--- Transmitter.ino
+++ Transmitter.ino
@@ -1,83 +1,100 @@
 #include <VirtualWire.h>
-#include <VirtualWire_Config.h>
-char *send;
-const int flex_pin1 = A0;
-const int flex_pin2 = A1;
-const int flex_pin3 = A2;
-const int flex_pin4 = A3;
-const float vcc = 4.98; 
-const float ext_resist = 100000.0;
-const float straight_flex_resist = 37300.0; 
-const float bend_flex_resist = 90000.0;
-void setup() {
-  pinMode(flex_pin1,INPUT);
-  pinMode(flex_pin2,INPUT);
-  pinMode(flex_pin3,INPUT);
-  pinMode(flex_pin4,INPUT);
-  pinMode(12,OUTPUT);
-  vw_set_ptt_inverted(true);
-  vw_set_tx_pin(12);
-  vw_setup(4000);
-}
+const int transmit_pin = 12;
+const int FLEX_PIN1 = A0; 
+const int FLEX_PIN2 = A1;
+const int FLEX_PIN3 = A2; 
+const int FLEX_PIN4 = A3; 
+const float VCC = 4.98;
+const float R_DIV = 47500.0;
+const float STRAIGHT_RESISTANCE1 = 35150.0; 
+const float STRAIGHT_RESISTANCE2 = 34150.0; 
+const float STRAIGHT_RESISTANCE3 = 37000.0; 
+const float STRAIGHT_RESISTANCE4 = 48000.0; 
+const float BEND_RESISTANCE1 = 74000.0; 
+const float BEND_RESISTANCE2 = 74000.0; 
+const float BEND_RESISTANCE3 = 74000.0; 
+const float BEND_RESISTANCE4 = 100000.0; 
+char i = 0;
 
-void loop() {
- int flexADC1 = analogRead(flex_pin1);
- int flexADC2 = analogRead(flex_pin2);
- int flexADC3 = analogRead(flex_pin3);
- int flexADC4 = analogRead(flex_pin4);
- float flexV1 = flexADC1 * vcc / 1023.0;
- float flexR1 = ext_resist * (vcc / flexV1 - 1.0);
- float flexV2 = flexADC2 * vcc / 1023.0;
- float flexR2 = ext_resist * (vcc / flexV2 - 1.0);
- float flexV3 = flexADC3 * vcc / 1023.0;
- float flexR3 = ext_resist * (vcc / flexV3 - 1.0);
- float flexV4 = flexADC4 * vcc / 1023.0;
- float flexR4 = ext_resist * (vcc / flexV4 - 1.0);
- float angle1 = map(flexR1, straight_flex_resist, bend_flex_resist, 0, 90.0);
- float angle2 = map(flexR2, straight_flex_resist, bend_flex_resist, 0, 90.0);
- float angle3 = map(flexR3, straight_flex_resist, bend_flex_resist, 0, 90.0);
- float angle4 = map(flexR4, straight_flex_resist, bend_flex_resist, 0, 90.0);
- if (angle1 > 20 && angle2 > 20)
- {
-  send="a";
-  vw_send((uint8_t *)send, strlen(send));
-  vw_wait_tx();
- }
- else if (angle3 > 20 && angle4 > 20)
- {
-  send="b";
-  vw_send((uint8_t *)send, strlen(send));
-  vw_wait_tx();
- }
- else if (angle1 > 20)
- {
-  send="c";
-  vw_send((uint8_t *)send, strlen(send));
-  vw_wait_tx();
- }
- else if (angle2 > 20)
- {
-  send="d";
-  vw_send((uint8_t *)send, strlen(send));
-  vw_wait_tx();
- }
- else if (angle3 > 20)
- {
-  send="e";
-  vw_send((uint8_t *)send, strlen(send));
-  vw_wait_tx();
- }
- else if (angle4 > 20)
- {
-  send="f";
-  vw_send((uint8_t *)send, strlen(send));
-  vw_wait_tx();
- }
- else
- {
-  send="g";
-  vw_send((uint8_t *)send, strlen(send));
-  vw_wait_tx();
- }
-delay(500);
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(FLEX_PIN1, INPUT);
+  pinMode(FLEX_PIN2, INPUT);
+  pinMode(FLEX_PIN3, INPUT);
+  pinMode(FLEX_PIN4, INPUT);  
+  vw_set_tx_pin(transmit_pin);
+  vw_set_ptt_pin(transmit_en_pin);
+  vw_set_ptt_inverted(true);
+  vw_setup(2000);
+}
+void loop()
+{
+  int flexADC1 = analogRead(FLEX_PIN1);
+  int flexADC2 = analogRead(FLEX_PIN2);
+  int flexADC3 = analogRead(FLEX_PIN3);
+  int flexADC4 = analogRead(FLEX_PIN4);
+  float flexV1 = flexADC1 * VCC / 1023.0;
+  float flexV2 = flexADC2 * VCC / 1023.0;
+  float flexV3 = flexADC3 * VCC / 1023.0;
+  float flexV4 = flexADC4 * VCC / 1023.0;
+  float flexR1 = R_DIV * (VCC / flexV1 - 1.0);
+  float flexR2 = R_DIV * (VCC / flexV2 - 1.0);
+  float flexR3 = R_DIV * (VCC / flexV3 - 1.0);
+  float flexR4 = R_DIV * (VCC / flexV4 - 1.0);
+  Serial.println("Resistance: " + String(flexR1) + " ohms");
+  Serial.println("Resistance: " + String(flexR2) + " ohms");
+  Serial.println("Resistance: " + String(flexR3) + " ohms");
+  Serial.println("Resistance: " + String(flexR4) + " ohms");
+  float angle1 = map(flexR1, STRAIGHT_RESISTANCE1, BEND_RESISTANCE1,0, 90.0);
+  float angle2 = map(flexR2, STRAIGHT_RESISTANCE2, BEND_RESISTANCE2,0, 90.0);
+  float angle3 = map(flexR3, STRAIGHT_RESISTANCE3, BEND_RESISTANCE3,0, 90.0);
+  float angle4 = map(flexR4, STRAIGHT_RESISTANCE4, BEND_RESISTANCE4,0, 90.0);
+  Serial.println("Bend: " + String(angle1) + " degrees");
+  Serial.println("Bend: " + String(angle2) + " degrees");
+  Serial.println("Bend: " + String(angle3) + " degrees");
+  Serial.println("Bend: " + String(angle4) + " degrees");
+  if (angle1 > 40 && angle2 > 40)
+  {
+   char msg[1] = {'a'};
+   vw_send((uint8_t *)msg,1);
+   vw_wait_tx();
+  }
+  else if (angle3 > 40 && angle4 > 65)
+  {
+   char msg[1] = {'b'};
+   vw_send((uint8_t *)msg,1);
+   vw_wait_tx();
+  }
+  else if (angle1 > 40)
+  {
+   char msg[1] = {'c'};
+   vw_send((uint8_t *)msg,1);
+   vw_wait_tx();
+  }
+  else if (angle2 > 40)
+  {
+   char msg[1] = {'d'};
+   vw_send((uint8_t *)msg,1);
+   vw_wait_tx();  
+  }
+  else if (angle3 > 40)
+  {
+   char msg[1] = {'f'};
+   vw_send((uint8_t *)msg,1);
+   vw_wait_tx();  
+  }
+  else if (angle4 > 65)
+  {
+   char msg[1] = {'e'};
+   vw_send((uint8_t *)msg,1);
+   vw_wait_tx();
+  }
+  else
+  {  
+   char msg[1] = {'z'};
+   vw_send((uint8_t *)msg,1);
+   vw_wait_tx();
+  }
+  delay(1000);
 }
