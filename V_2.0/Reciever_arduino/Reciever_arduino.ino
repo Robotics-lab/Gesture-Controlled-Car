eca020b88248dd89029b59238e447d81a3ad404e
Rohan <avaids.r@gmail.com>
2018-09-27 09:51:05 +0530

    version 2.0 with accelerometer

---
+++ V_2.0/Reciever_arduino/Reciever_arduino.ino
@@ -0,0 +1,55 @@
+//Reciever
+char a;
+
+void setup() {
+  pinMode(5,OUTPUT); //Motor Pin
+  pinMode(6,OUTPUT); //Motor Pin
+  pinMode(7,OUTPUT); //Motor Pin
+  pinMode(8,OUTPUT); //Motor Pin
+
+}
+
+void loop() {
+  //Your Code here
+
+  //Recive value in 'a' variable
+
+  switch(a):
+  case W: forward();
+  break;
+  case S:backward();
+  break;
+  case A:left();
+  break;
+  case D:right();
+
+}
+
+void forward(){
+  digitalWrite(5,1);
+  digitalWrite(6,0);
+  digitalWrite(7,1);
+  digitalWrite(8,0);
+}
+
+void backward() {
+  digitalWrite(5,0);
+  digitalWrite(6,1);
+  digitalWrite(7,0);
+  digitalWrite(8,1);
+}
+
+void left() {
+  digitalWrite(5,1);
+  digitalWrite(6,0);
+  digitalWrite(7,0);
+  digitalWrite(8,1);
+}
+
+void right() {
+  digitalWrite(5,0);
+  digitalWrite(6,1);
+  digitalWrite(7,1);
+  digitalWrite(8,0);
+}
+
---
+++ V_2.0/Transmitter_arduino/Transmitter_arduino.ino
@@ -0,0 +1,65 @@
+//Transmitter
+
+int flex_val = 0; //flex enable pin
+int x_ax = 0; //X-axis value
+int y_ax = 0; //Y-axis value
+
+void setup() {
+  pinMode(A1,INPUT); //Flex Sensor
+  pinMode(A2,INPUT); //Accelerometer X-Axis
+  pinMode(A3,INPUT); //Accelerometer Y-Axis
+
+}
+
+void loop() {
+  flex_val = analogRead(A1);
+  
+
+  if (flex_val > [Threshhold]) {
+    x_ax = analogRead(A2);
+    y_ax = analogRead(A3);
+
+    if (x_ax > [+ve Threshhold]) {
+      //Forward
+      //Your code here
+      
+      //Transmit value 'W'
+    }
+
+    if (x_ax < [-ve Threshhold]) {
+      //Backward
+      //Your code here
+      
+      //Transmit value 'S'
+    }
+
+    if (y_ax > [+ve Threshhold]) {
+      //Left
+      //Your code here
+      
+      //Transmit value 'A'
+    }
+
+    if (y_ax < [-ve Threshhold]) {
+      //Right
+      //Your code here
+      
+      //Transmit value 'D'
+    }
+
+    if ((y_ax > [+ve Threshhold])&&(x_ax > [+ve Threshhold])) {
+      //Left
+      //Your code here
+      
+      //Transmit value 'A'
+    }
+
+    if ((y_ax < [-ve Threshhold])&&(x_ax > [+ve Threshhold])) {
+      //Right
+      //Your code here
+      
+      //Transmit value 'D'
+    }
+  }
+
+}
